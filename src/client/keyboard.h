25061c16de9844f29fcc0d2fad5222d11bf9cbd2
Martin Gräßlin <mgraesslin@kde.org>
2014-09-23 09:10:07 +0200

    Add documentation for Keyboard

--- src/client/keyboard.h
+++ src/client/keyboard.h
@@ -31,6 +31,15 @@ namespace KWayland
 namespace Client
 {
 
+/**
+ * @short Wrapper for the wl_keyboard interface.
+ *
+ * This class is a convenient wrapper for the wl_keyboard interface.
+ *
+ * To create an instance use Seat::createKeyboard.
+ *
+ * @see Seat
+ **/
 class KWAYLANDCLIENT_EXPORT Keyboard : public QObject
 {
     Q_OBJECT
@@ -42,17 +51,67 @@ public:
     explicit Keyboard(QObject *parent = nullptr);
     virtual ~Keyboard();
 
+    /**
+     * @returns @c true if managing a wl_keyboard.
+     **/
     bool isValid() const;
+    /**
+     * Setup this Keyboard to manage the @p keyboard.
+     * When using Seat::createKeyboard there is no need to call this
+     * method.
+     **/
     void setup(wl_keyboard *keyboard);
+    /**
+     * Releases the wl_keyboard interface.
+     * After the interface has been released the Keyboard instance is no
+     * longer valid and can be setup with another wl_keyboard interface.
+     *
+     * This method is automatically invoked when the Seat which created this
+     * Keyboard gets released.
+     **/
     void release();
+    /**
+     * Destroys the data hold by this Keyboard.
+     * This method is supposed to be used when the connection to the Wayland
+     * server goes away. If the connection is not valid any more, it's not
+     * possible to call release any more as that calls into the Wayland
+     * connection and the call would fail. This method cleans up the data, so
+     * that the instance can be deleted or setup to a new wl_keyboard interface
+     * once there is a new connection available.
+     *
+     * This method is automatically invoked when the Seat which created this
+     * Keyboard gets destroyed.
+     *
+     * @see release
+     **/
     void destroy();
 
     operator wl_keyboard*();
     operator wl_keyboard*() const;
 
 Q_SIGNALS:
+    /**
+     * This signal provides a file descriptor to the client which can
+     * be memory-mapped to provide a keyboard mapping description.
+     *
+     * The signal is only emitted if the keymap format is libxkbcommon compatible.
+     *
+     * @param fd file descriptor of the keymap
+     * @param size The size of the keymap
+     **/
     void keymapChanged(int fd, quint32 size);
+    /**
+     * A key was pressed or released.
+     * The time argument is a timestamp with millisecond granularity, with an undefined base.
+     * @param key The key which was pressed
+     * @param state Whether the key got @c Released or @c Pressed
+     * @param time The timestamp
+     **/
     void keyChanged(quint32 key, KWayland::Client::Keyboard::KeyState state, quint32 time);
+    /**
+     * Notifies clients that the modifier and/or group state has changed,
+     * and it should update its local state.
+     **/
     void modifiersChanged(quint32 depressed, quint32 latched, quint32 locked, quint32 group);
 
 private:

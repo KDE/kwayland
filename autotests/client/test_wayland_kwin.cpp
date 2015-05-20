094f7a0725e000211645fe492efdbc06121e8f45
Sebastian Kügler <sebas@kde.org>
2015-05-20 04:50:25 +0200

    autotest skeleton for kwin client side

---
+++ autotests/client/test_wayland_kwin.cpp
@@ -0,0 +1,117 @@
+/********************************************************************
+Copyright 2014  Martin Gräßlin <mgraesslin@kde.org>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) version 3, or any
+later version accepted by the membership of KDE e.V. (or its
+successor approved by the membership of KDE e.V.), which shall
+act as a proxy defined in Section 6 of version 3 of the license.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+// Qt
+#include <QtTest/QtTest>
+// KWin
+#include "../../src/client/connection_thread.h"
+#include "../../src/client/event_queue.h"
+#include "../../src/client/output.h"
+#include "../../src/client/registry.h"
+#include "../../src/server/display.h"
+#include "../../src/server/output_interface.h"
+//#include "../../src/server/kwin_interface.h"
+// Wayland
+#include <wayland-client-protocol.h>
+
+class TestWaylandKWin : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TestWaylandKWin(QObject *parent = nullptr);
+private Q_SLOTS:
+    void init();
+    void cleanup();
+
+    void testGetOutputs();
+
+private:
+    KWayland::Server::Display *m_display;
+//     KWayland::Server::KWin *m_kwin;
+    KWayland::Client::ConnectionThread *m_connection;
+    KWayland::Client::EventQueue *m_queue;
+    QThread *m_thread;
+};
+
+static const QString s_socketName = QStringLiteral("kwin-test-wayland-output-0");
+
+TestWaylandKWin::TestWaylandKWin(QObject *parent)
+    : QObject(parent)
+    , m_display(nullptr)
+    , m_connection(nullptr)
+    , m_thread(nullptr)
+{
+}
+
+void TestWaylandKWin::init()
+{
+    using namespace KWayland::Server;
+    delete m_display;
+    m_display = new Display(this);
+    m_display->setSocketName(s_socketName);
+    m_display->start();
+    QVERIFY(m_display->isRunning());
+    m_display->createKWin(this);
+
+    // setup connection
+    m_connection = new KWayland::Client::ConnectionThread;
+    QSignalSpy connectedSpy(m_connection, SIGNAL(connected()));
+    m_connection->setSocketName(s_socketName);
+
+    m_thread = new QThread(this);
+    m_connection->moveToThread(m_thread);
+    m_thread->start();
+
+    m_connection->initConnection();
+    QVERIFY(connectedSpy.wait());
+
+    m_queue = new KWayland::Client::EventQueue(this);
+    QVERIFY(!m_queue->isValid());
+    m_queue->setup(m_connection);
+    QVERIFY(m_queue->isValid());
+}
+
+void TestWaylandKWin::cleanup()
+{
+    if (m_queue) {
+        delete m_queue;
+        m_queue = nullptr;
+    }
+    if (m_thread) {
+        m_thread->quit();
+        m_thread->wait();
+        delete m_thread;
+        m_thread = nullptr;
+    }
+    delete m_connection;
+    m_connection = nullptr;
+
+    delete m_display;
+    m_display = nullptr;
+}
+
+void TestWaylandKWin::testGetOutputs()
+{
+     /*-*/
+}
+
+
+
+QTEST_GUILESS_MAIN(TestWaylandKWin)
+#include "test_wayland_kwin.moc"

4558e2875cd85cffdc8e463e5e4257e0a3e9807a
Marco Martin <notmart@gmail.com>
2015-08-06 18:11:56 +0200

    first skeleton of plasmaeffects implementation

---
+++ src/client/plasmaeffects.cpp
@@ -0,0 +1,113 @@
+/********************************************************************
+Copyright 2015 Marco Martin <mart@kde.org>
+Copyright 2015 Martin Gräßlin <mgraesslin@kde.org>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) version 3, or any
+later version accepted by the membership of KDE e.V. (or its
+successor approved by the membership of KDE e.V.), which shall
+act as a proxy defined in Section 6 of version 3 of the license.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#include "plasmaeffects.h"
+#include "event_queue.h"
+#include "surface.h"
+#include "region.h"
+#include "wayland_pointer_p.h"
+
+#include <QMarginsF>
+
+#include <wayland-plasma-effects-client-protocol.h>
+
+namespace KWayland
+{
+
+namespace Client
+{
+
+class PlasmaEffects::Private
+{
+public:
+    Private() = default;
+
+    WaylandPointer<org_kde_plasma_effects, org_kde_plasma_effects_destroy> effects;
+    EventQueue *queue = nullptr;
+};
+
+PlasmaEffects::PlasmaEffects(QObject *parent)
+    : QObject(parent)
+    , d(new Private)
+{
+}
+
+PlasmaEffects::~PlasmaEffects()
+{
+    release();
+}
+
+void PlasmaEffects::release()
+{
+    d->effects.release();
+}
+
+void PlasmaEffects::destroy()
+{
+    d->effects.destroy();
+}
+
+bool PlasmaEffects::isValid() const
+{
+    return d->effects.isValid();
+}
+
+void PlasmaEffects::setup(org_kde_plasma_effects *effects)
+{
+    Q_ASSERT(effects);
+    Q_ASSERT(!d->effects);
+    d->effects.setup(effects);
+}
+
+void PlasmaEffects::setEventQueue(EventQueue *queue)
+{
+    d->queue = queue;
+}
+
+EventQueue *PlasmaEffects::eventQueue()
+{
+    return d->queue;
+}
+
+void PlasmaEffects::setBlurBehindRegion(Surface *surface, const Region *region)
+{
+    Q_ASSERT(isValid());
+    Q_ASSERT(surface);
+
+    if (region) {
+        org_kde_plasma_effects_set_blur_behind_region(d->effects, *surface, *region);
+    } else {
+        org_kde_plasma_effects_set_blur_behind_region(d->effects, *surface, nullptr);
+    }
+}
+
+PlasmaEffects::operator org_kde_plasma_effects*()
+{
+    return d->effects;
+}
+
+PlasmaEffects::operator org_kde_plasma_effects*() const
+{
+    return d->effects;
+}
+
+}
+}
---
+++ src/client/plasmaeffects.h
@@ -0,0 +1,120 @@
+/********************************************************************
+Copyright 2015 Marco Martin <mart@kde.org>
+Copyright 2015 Martin Gräßlin <mgraesslin@kde.org>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) version 3, or any
+later version accepted by the membership of KDE e.V. (or its
+successor approved by the membership of KDE e.V.), which shall
+act as a proxy defined in Section 6 of version 3 of the license.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+#ifndef KWAYLAND_PLASMAEFFECTS_H
+#define KWAYLAND_PLASMAEFFECTS_H
+
+#include "buffer.h"
+
+#include <QObject>
+#include <QPoint>
+#include <QSize>
+
+#include <KWayland/Client/kwaylandclient_export.h>
+
+struct wl_buffer;
+struct org_kde_plasma_effects;
+
+class QMarginsF;
+class QWindow;
+
+namespace KWayland
+{
+namespace Client
+{
+
+class EventQueue;
+class Surface;
+class Region;
+
+class KWAYLANDCLIENT_EXPORT PlasmaEffects : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit PlasmaEffects(QObject *parent = nullptr);
+    virtual ~PlasmaEffects();
+
+    /**
+     * @returns @c true if managing a org_kde_plasma_effects.
+     **/
+    bool isValid() const;
+    /**
+     * Setup this PlasmaEffects to manage the @p effects.
+     * When using Registry::createPlasmaEffects there is no need to call this
+     * method.
+     **/
+    void setup(org_kde_plasma_effects *effects);
+    /**
+     * Releases the org_kde_plasma_effects interface.
+     * After the interface has been released the PlasmaEffects instance is no
+     * longer valid and can be setup with another org_kde_plasma_effects interface.
+     **/
+    void release();
+    /**
+     * Destroys the data hold by this PlasmaEffects.
+     * This method is supposed to be used when the connection to the Wayland
+     * server goes away. If the connection is not valid any more, it's not
+     * possible to call release any more as that calls into the Wayland
+     * connection and the call would fail. This method cleans up the data, so
+     * that the instance can be deleted or setup to a new org_kde_plasma_effects interface
+     * once there is a new connection available.
+     *
+     * It is suggested to connect this method to ConnectionThread::connectionDied:
+     * @code
+     * connect(connection, &ConnectionThread::connectionDied, compositor, &PlasmaEffects::destroyed);
+     * @endcode
+     *
+     * @see release
+     **/
+    void destroy();
+
+    /**
+     * Sets the @p queue to use for creating a PlasmaEffects.
+     **/
+    void setEventQueue(EventQueue *queue);
+    /**
+     * @returns The event queue to use for creating a PlasmaEffects.
+     **/
+    EventQueue *eventQueue();
+
+    /**
+     * This request sets the region of the surface that will allow to see
+     * through with a blur effect.
+     * Pass a null region to disable blur behind.
+     *
+     * @param surface The surface we want apply the blur behind effect to
+     * @param region The region of the surface we want apply the blur behind effect to
+     */
+    void setBlurBehindRegion(Surface *surface, const Region *region = nullptr);
+
+    operator org_kde_plasma_effects*();
+    operator org_kde_plasma_effects*() const;
+
+private:
+    class Private;
+    QScopedPointer<Private> d;
+};
+
+}
+}
+
+#endif
+

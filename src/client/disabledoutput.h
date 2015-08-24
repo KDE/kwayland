14ce191d13d3d5be5dacab0e6d7a9b3e0a225d34
Sebastian Kügler <sebas@kde.org>
2015-08-25 00:01:35 +0200

    Private ctor and setters for K::C::DisabledOutput
    friend ScreenManagement instead, since that's the only place where these
    guys should be created.

--- src/client/disabledoutput.h
+++ src/client/disabledoutput.h
@@ -36,52 +36,39 @@ namespace KWayland
 namespace Client
 {
 
+class ScreenManagement;
 /**
- * @short Wrapper for the wl_output interface.
- *
- * This class provides a convenient wrapper for the wl_output interface.
- * Its main purpose is to hold the information about one DisabledOutput.
- *
- * To use this class one needs to interact with the Registry. There are two
- * possible ways to create an DisabledOutput interface:
- * @code
- * DisabledOutput *c = registry->createDisabledOutput(name, version);
- * @endcode
- *
- * This creates the DisabledOutput and sets it up directly. As an alternative this
- * can also be done in a more low level way:
- * @code
- * DisabledOutput *c = new DisabledOutput;
- * c->setup(registry->bindDisabledOutput(name, version));
- * @endcode
- *
- * The DisabledOutput can be used as a drop-in replacement for any wl_output
- * pointer as it provides matching cast operators.
- *
- * Please note that all properties of DisabledOutput are not valid until the
- * changed signal has been emitted. The wayland server is pushing the
- * information in an async way to the DisabledOutput instance. By emitting changed
- * the DisabledOutput indicates that all relevant information is available.
- *
- * @see Registry
+ * @short Wrapper for the connected, but disabled outputs.
+ * @see ScreenManagement
  **/
 class KWAYLANDCLIENT_EXPORT DisabledOutput : public QObject
 {
     Q_OBJECT
 public:
-    explicit DisabledOutput(QObject *parent = nullptr);
     virtual ~DisabledOutput();
 
+    /**
+     * @return EDID information of the output as QString
+     */
     QString edid() const;
-    void setEdid(const QString &e);
 
+    /**
+     * @return The name of the output
+     */
     QString name() const;
-    void setName(const QString &n);
 
+    /**
+     * @return The connector of the output
+     */
     QString connector() const;
-    void setConnector(const QString &c);
 
 private:
+    friend class ScreenManagement;
+    explicit DisabledOutput(QObject *parent = nullptr);
+    void setConnector(const QString &c);
+    void setEdid(const QString &e);
+    void setName(const QString &n);
+
     class Private;
     QScopedPointer<Private> d;
 };

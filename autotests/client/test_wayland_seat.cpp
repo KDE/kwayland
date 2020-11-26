78f3c8ad6b19dff8ac6cbf331d41f364af0b00aa
Laurent Montel <montel@kde.org>
2020-11-26 19:52:22 +0100

    Compile without deprecated method
    Qt::MidButton is equal to Qt::MiddleButton and it was removed in qt6

--- autotests/client/test_wayland_seat.cpp
+++ autotests/client/test_wayland_seat.cpp
@@ -719,7 +719,6 @@ void TestWaylandSeat::testPointerButton_data()
 
     QTest::newRow("left")    << Qt::LeftButton    << quint32(BTN_LEFT);
     QTest::newRow("right")   << Qt::RightButton   << quint32(BTN_RIGHT);
-    QTest::newRow("mid")     << Qt::MidButton     << quint32(BTN_MIDDLE);
     QTest::newRow("middle")  << Qt::MiddleButton  << quint32(BTN_MIDDLE);
     QTest::newRow("back")    << Qt::BackButton    << quint32(BTN_BACK);
     QTest::newRow("x1")      << Qt::XButton1      << quint32(BTN_BACK);

9e9c7daf8c21521453e3e46b32f39120b3e0473a
Ömer Fadıl Usta <omerusta@gmail.com>
2021-04-29 17:59:08 +0000

    Port QMutex -> QRecursiveMutex

--- src/client/connection_thread.cpp
+++ src/client/connection_thread.cpp
@@ -42,14 +42,14 @@ public:
     QMetaObject::Connection eventDispatcherConnection;
     int error = 0;
     static QVector<ConnectionThread *> connections;
-    static QMutex mutex;
+    static QRecursiveMutex mutex;
 
 private:
     ConnectionThread *q;
 };
 
 QVector<ConnectionThread *> ConnectionThread::Private::connections = QVector<ConnectionThread *>{};
-QMutex ConnectionThread::Private::mutex{QMutex::Recursive};
+QRecursiveMutex ConnectionThread::Private::mutex;
 
 ConnectionThread::Private::Private(ConnectionThread *q)
     : socketName(QString::fromUtf8(qgetenv("WAYLAND_DISPLAY")))

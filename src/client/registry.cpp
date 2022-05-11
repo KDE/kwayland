0049bd064a5578630e3c5618d43ecbf979a8b70f
David Redondo <kde@david-redondo.de>
2022-05-11 12:16:40 +0200

    client: Bump plasmashell version to 7
    BUG:453578
    (cherry picked from commit 2e94a425120971ec535278629e216f5e632fe9bc)

--- src/client/registry.cpp
+++ src/client/registry.cpp
@@ -158,7 +158,7 @@ static const QMap<Registry::Interface, SuppertedInterfaceData> s_interfaces = {
         &Registry::subCompositorRemoved
     }},
     {Registry::Interface::PlasmaShell, {
-        6,
+        7,
         QByteArrayLiteral("org_kde_plasma_shell"),
         &org_kde_plasma_shell_interface,
         &Registry::plasmaShellAnnounced,

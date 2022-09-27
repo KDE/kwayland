KDE/kwayland

commit cb13afd8530f651c7d0ce82e47eaa3e1939bd676

Add support for wl_output version 4

src/client/output.cpp
@@ -39,6 +39,8 @@ public:
     Transform transform = Transform::Normal;
     Modes modes;
     Modes::iterator currentMode = modes.end();
+    QString name;
+    QString description;
 
     static Output *get(wl_output *o);
 
@@ -56,6 +58,8 @@ private:
     static void modeCallback(void *data, wl_output *output, uint32_t flags, int32_t width, int32_t height, int32_t refresh);
     static void doneCallback(void *data, wl_output *output);
     static void scaleCallback(void *data, wl_output *output, int32_t scale);
+    static void nameCallback(void *data, struct wl_output *wl_output, const char *name);
+    static void descriptionCallback(void *data, struct wl_output *wl_output, const char *description);
     void setPhysicalSize(const QSize &size);
     void setGlobalPosition(const QPoint &pos);
     void setManufacturer(const QString &manufacturer);
@@ -120,7 +124,7 @@ Output::~Output()
     d->output.release();
 }
 
-wl_output_listener Output::Private::s_outputListener = {geometryCallback, modeCallback, doneCallback, scaleCallback};
+wl_output_listener Output::Private::s_outputListener = {geometryCallback, modeCallback, doneCallback, scaleCallback, nameCallback, descriptionCallback};
 
 void Output::Private::geometryCallback(void *data,
                                        wl_output *output,
@@ -232,6 +236,20 @@ void Output::Private::scaleCallback(void *data, wl_output *output, int32_t scale
     auto o = reinterpret_cast<Output::Private *>(data);
     Q_ASSERT(o->output == output);
     o->setScale(scale);
+}
+
+void Output::Private::nameCallback(void *data, struct wl_output *wl_output, const char *name)
+{
+    auto o = reinterpret_cast<Output::Private *>(data);
+    Q_ASSERT(o->output == wl_output);
+    o->name = QString::fromUtf8(name);
+}
+
+void Output::Private::descriptionCallback(void *data, struct wl_output *wl_output, const char *description)
+{
+    auto o = reinterpret_cast<Output::Private *>(data);
+    Q_ASSERT(o->output == wl_output);
+    o->description = QString::fromUtf8(description);
 }
 
 void Output::Private::doneCallback(void *data, wl_output *output)
@@ -363,6 +381,16 @@ Output::Transform Output::transform() const
 QList<Output::Mode> Output::modes() const
 {
     return d->modes;
+}
+
+QString Output::name() const
+{
+    return d->name;
+}
+
+QString Output::description() const
+{
+    return d->description;
 }
 
 Output::operator wl_output *()

src/client/output.h
@@ -175,6 +175,16 @@ public:
     QList<Mode> modes() const;
 
     /**
+     * Returns the name of the output.
+     **/
+    QString name() const;
+
+    /**
+     * Returns the human readable description of the output.
+     **/
+    QString description() const;
+
+    /**
      * Sets the @p queue to use for bound proxies.
      **/
     void setEventQueue(EventQueue *queue);

src/client/registry.cpp
@@ -123,7 +123,7 @@ static const QMap<Registry::Interface, SuppertedInterfaceData> s_interfaces = {
         &Registry::dataDeviceManagerRemoved
     }},
     {Registry::Interface::Output, {
-        3,
+        4,
         QByteArrayLiteral("wl_output"),
         &wl_output_interface,
         &Registry::outputAnnounced,

commit 62434fce536e959746d727e2dc7cc1e26f919f92

Fix compile error with wl_output < v4
Listener members 'name' and 'description' of struct wl_output_listener
were added in version 4. With older versions it would therefore cause
a compiler error.

Check whether macros WL_OUTPUT_NAME_SINCE_VERSION and
WL_OUTPUT_DESCRIPTION_SINCE_VERSION are defined to assume whether
or not those two listener members exist.

Fixes: cb13afd8530f651c7d0ce82e47eaa3e1939bd676

src/client/output.cpp
@@ -124,7 +124,15 @@ Output::~Output()
     d->output.release();
 }
 
-wl_output_listener Output::Private::s_outputListener = {geometryCallback, modeCallback, doneCallback, scaleCallback, nameCallback, descriptionCallback};
+wl_output_listener Output::Private::s_outputListener = {
+    geometryCallback, modeCallback, doneCallback, scaleCallback,
+#ifdef WL_OUTPUT_NAME_SINCE_VERSION
+    nameCallback,
+#endif
+#ifdef WL_OUTPUT_DESCRIPTION_SINCE_VERSION
+    descriptionCallback,
+#endif
+};
 
 void Output::Private::geometryCallback(void *data,
                                        wl_output *output,